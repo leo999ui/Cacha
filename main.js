0cf8f957e4f9f14105e99b640dcdcb1d121e27e5
leo999ui <Akramsead999@gmail.com>
2025-07-14 04:27:04 +0300

    update

---
+++ main.js
@@ -0,0 +1,33 @@
+
+ 
+ let freinds =[
+   {
+    firstname: "ahmed",
+    lastname: "john",
+    id: "hhhgh",
+    
+    
+   },
+   {
+    firstname: "amed",
+    lastname: "jhn",
+    id: "hhghu",
+    
+    
+   }
+   ,{
+    firstname: "sein",
+    lastname: "joh",
+    id: "hhhhu",
+    
+    
+   }
+ ];
+ console.log(freinds)
+ let hoppy = "dancing"
+ 
+ if (hoppy !== "coding"){
+  console.log("**i love codeng**")
+ } else{
+  console.log("**can you teach me**")
+ }
